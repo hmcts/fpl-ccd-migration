823ed38380fdca88f6b35a194cf41787ba935bca
Damian Dunajski <damian.dunajski@hmcts.net>
2019-06-26 09:59:44 +0100

    Rewrite to make project easier to extend (#8)

--- src/main/java/uk.gov.hmcts.reform.fpl.ccddatamigration/ccd/CcdUpdateService.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.fpl.ccddatamigration.ccd;
-
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-
-public interface CcdUpdateService {
-    CaseDetails update(String caseId, Object data, String eventId, String authorisation,
-                       String eventSummary, String eventDescription);
-}
