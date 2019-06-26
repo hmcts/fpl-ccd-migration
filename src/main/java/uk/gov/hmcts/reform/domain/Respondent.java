74bc7aaa2b5a5caa94b9a412c248ecc3797c7f19
Damian Dunajski <damdun@gmail.com>
2019-06-26 13:23:15 +0100

    Move package around

--- src/main/java/uk/gov/hmcts/reform/domain/Respondent.java
+++ src/main/java/uk/gov/hmcts/reform/domain/Respondent.java
@@ -1,9 +1,9 @@
-package uk.gov.hmcts.reform.fpl.ccddatamigration.domain;
+package uk.gov.hmcts.reform.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import uk.gov.hmcts.reform.fpl.ccddatamigration.domain.common.Party;
+import uk.gov.hmcts.reform.domain.common.Party;
 
 @Data
 @Builder
--- src/main/java/uk/gov/hmcts/reform/domain/common/Address.java
+++ src/main/java/uk/gov/hmcts/reform/domain/common/Address.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.fpl.ccddatamigration.domain.common;
+package uk.gov.hmcts.reform.domain.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
