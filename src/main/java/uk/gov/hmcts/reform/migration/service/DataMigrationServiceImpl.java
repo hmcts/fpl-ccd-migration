e99ef7ec1ce168249f6b1c1fb6c7f33af4534afc
Joe Zazzaro-Francis <joez@kainos.com>
2020-09-08 10:36:03 +0100

    Return empty map

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -3,9 +3,6 @@ package uk.gov.hmcts.reform.migration.service;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -24,6 +21,6 @@ public class DataMigrationServiceImpl implements DataMigrationService<Object> {
     @SuppressWarnings("unchecked")
     @Override
     public Object migrate(Map<String, Object> data) {
-        return data;
+        return Map.of();
     }
 }
