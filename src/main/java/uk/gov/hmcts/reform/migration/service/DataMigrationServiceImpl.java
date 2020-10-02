1517f1aa21b4ca4a79fcb66c91facb88129333df
James Nelson <Jamesnelson117@gmail.com>
2020-10-02 13:29:25 +0100

    FPLA-2262: Added service annoation

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.migration.service;
 
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+@Service
 public class DataMigrationServiceImpl implements DataMigrationService<Object> {
     @Override
     public Predicate<CaseDetails> accepts() {
