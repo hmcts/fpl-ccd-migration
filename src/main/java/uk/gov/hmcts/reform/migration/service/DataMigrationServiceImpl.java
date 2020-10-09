1f02aa633d8ef1b2139a55ee220260f16cdc8dcd
James Nelson <Jamesnelson117@gmail.com>
2020-10-09 10:50:47 +0100

    FPLA-2271: Removes second sealed CMO from case SA20C50002

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Service
+public class DataMigrationServiceImpl implements DataMigrationService<Object> {
+    @Override
+    public Predicate<CaseDetails> accepts() {
+        return caseDetails -> Optional.ofNullable(caseDetails)
+            .map(CaseDetails::getData)
+            .filter(data -> data.getOrDefault("familyManCaseNumber", "").equals("SA20C50002"))
+            .isPresent();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Object migrate(Map<String, Object> data) {
+        return new HashMap<>();
+    }
+}
