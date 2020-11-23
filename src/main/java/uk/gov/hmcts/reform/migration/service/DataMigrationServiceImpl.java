c16dae9f0665c699354389ccfebb62708d591ee0
Lewis Birks <lewisb@kainos.com>
2020-11-23 11:20:44 +0000

    migration for ZW20C50003

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+public class DataMigrationServiceImpl implements DataMigrationService<Object>{
+
+    @Override
+    public Predicate<CaseDetails> accepts() {
+        return caseDetails -> Optional.ofNullable(caseDetails)
+            .map(CaseDetails::getData)
+            .filter(data -> "ZW20C50003".equals(data.get("familyManCaseNumber")))
+            .isPresent();
+    }
+
+    @Override
+    public Object migrate(Map<String, Object> data) {
+        return new HashMap<>();
+    }
+}
