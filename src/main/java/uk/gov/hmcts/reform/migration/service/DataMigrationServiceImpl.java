e77f432dfcce9c20b1a10f45157745e658fcdc8f
Lewis Birks <lewisb@kainos.com>
2020-11-23 11:44:34 +0000

    migration for CF20C50024

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+public class DataMigrationServiceImpl implements DataMigrationService<Object> {
+
+    @Override
+    public Predicate<CaseDetails> accepts() {
+        return caseDetails -> Optional.ofNullable(caseDetails)
+                                      .map(CaseDetails::getData)
+                                      .filter(data -> "CF20C50024".equals(
+                                          data.get("familyManCaseNumber")))
+                                      .isPresent();
+    }
+
+    @Override
+    public Object migrate(Map<String, Object> data) {
+        return new HashMap<>();
+    }
+}
