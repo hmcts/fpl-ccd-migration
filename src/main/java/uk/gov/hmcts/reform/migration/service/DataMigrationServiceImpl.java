28dd4577994b53b58e4f410f7114c3b9ffa45aa3
James Nelson <Jamesnelson117@gmail.com>
2021-02-02 13:20:20 +0000

    FPLA-2654: Removal of draft CMO

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Service
+public class DataMigrationServiceImpl implements DataMigrationService<Object> {
+
+    @Override
+    public Predicate<CaseDetails> accepts() {
+        return caseDetails -> Optional.ofNullable(caseDetails)
+            .map(CaseDetails::getData)
+            .filter(data -> "NE20C50011".equals(data.get("familyManCaseNumber")))
+            .isPresent();
+    }
+
+    @Override
+    public Object migrate(Map<String, Object> data) {
+        return Map.<String, Object>of("migrationId", "FPLA-2654");
+    }
+}
