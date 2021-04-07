310c0d6cabce2a354e4b88b32622788a4a0b944a
Imran Saeed <muhamed.imran@hmcts.net>
2021-04-07 15:05:08 +0100

    bugfix:fpla-2947
    Set migration condition to the cases which failed due to NPE

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -3,41 +3,21 @@ package uk.gov.hmcts.reform.migration.service;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 @Service
 public class DataMigrationServiceImpl implements DataMigrationService<Object> {
-    List<String> migratedListCodes = List.of("OT8", "OT9", "OT10");
+    List<Long> caseIds = List.of(1602246223743823L, 1611588537917646L);
 
     @Override
     public Predicate<CaseDetails> accepts() {
-        return this::caseContainsMigratedCancellationReasons;
-    }
-
-    private boolean caseContainsMigratedCancellationReasons(CaseDetails caseDetails) {
-        Optional<List<String>> cancellationReasonList = Optional.ofNullable(caseDetails)
-            .map(CaseDetails::getData)
-            .map(data -> data.get("cancelledHearingDetails"))
-            .map(hearingList -> (List<Map<String, Object>>) hearingList)
-            .map(hearingList -> hearingList.stream()
-                .map(hearingListItem -> {
-                    Map<String, Object> value = (Map<String, Object>) hearingListItem.get("value");
-                    return (String) value.get("cancellationReason");
-                }).collect(Collectors.toList()));
-
-        List<String> cancellationReasons = cancellationReasonList.orElse(new ArrayList<>());
-
-        return !Collections.disjoint(cancellationReasons, migratedListCodes);
+        return caseDetails -> caseIds.contains(caseDetails.getId());
     }
 
     @Override
     public Object migrate(Map<String, Object> data) {
-        return Map.<String, Object>of("migrationId", "FPLA-2885");
+        return Map.<String, Object>of("migrationId", "FPLA-2947");
     }
 }
