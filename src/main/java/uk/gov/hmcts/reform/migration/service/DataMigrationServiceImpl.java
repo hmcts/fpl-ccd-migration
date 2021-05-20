f4d7df10b59e8548073391b512a58505702e98ce
Soorya <soorya11@hotmail.co.uk>
2021-05-20 16:12:09 +0100

    * Initial commit

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+@Service
+public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
+    List<Long> caseIds = List.of(1616408548410918L);
+
+    @Override
+    public Predicate<CaseDetails> accepts() {
+        return caseDetails -> caseIds.contains(caseDetails.getId());
+    }
+
+    @Override
+    public Map<String, Object> migrate(Map<String, Object> data) {
+        return Map.of("migrationId", "FPLA-3037");
+    }
+}
