a739ed79492b314fd1f2ed7ed8027be59ebad292
aliveni <aliveni.ch@gmail.com>
2021-06-15 15:01:22 +0100

    FPLA-3135 remove case document

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
+@Service
+public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
+    private static final String FAMILY_MAN_ID = "DE21C50016";
+
+    public Predicate<CaseDetails> accepts() {
+        return caseDetails -> FAMILY_MAN_ID.equals(caseDetails.getData().get("familyManCaseNumber"));
+    }
+
+    public Map<String, Object> migrate(Map<String, Object> data) {
+        return Map.of("migrationId", "FPLA-3135");
+    }
+}
