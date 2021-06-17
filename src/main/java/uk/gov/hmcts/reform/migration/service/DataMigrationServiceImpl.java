0e269edb9da05c01fb029116ad6dfaf4768eaada
aliveni <aliveni.ch@gmail.com>
2021-06-17 16:44:49 +0100

    FPLA-3088 remove C2 application from CF21C50022

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -8,13 +8,13 @@ import java.util.function.Predicate;
 
 @Service
 public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
-    private static final String FAMILY_MAN_ID = "CF20C50063";
+    private static final String FAMILY_MAN_ID = "CF21C50022";
 
     public Predicate<CaseDetails> accepts() {
         return caseDetails -> FAMILY_MAN_ID.equals(caseDetails.getData().get("familyManCaseNumber"));
     }
 
     public Map<String, Object> migrate(Map<String, Object> data) {
-        return Map.of("migrationId", "FPLA-3092");
+        return Map.of("migrationId", "FPLA-3088");
     }
 }
