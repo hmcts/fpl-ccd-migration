6e9ba268f5f73567471655bf35ae82ee14a33554
Joe Zazzaro-Francis <joez@kainos.com>
2021-07-19 23:32:47 +0100

    fpla-3216

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,23 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
+@Service
+public class DataMigrationServiceImpl implements DataMigrationService<Object> {
+
+    @Override
+    public Predicate<CaseDetails> accepts() {
+        return caseDetails -> caseDetails.getData()
+            .getOrDefault("familyManCaseNumber", "")
+            .equals("NE21C50026");
+    }
+
+    @Override
+    public Object migrate(Map<String, Object> data) {
+        return Map.<String, Object>of("migrationId", "FPLA-3216");
+    }
+}
