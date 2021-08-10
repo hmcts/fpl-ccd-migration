888159d6f8d498931a369867cef76ba066c2d6ef
Lewis Birks <lewisb@kainos.com>
2021-08-10 13:30:13 +0100

    add a base migration service that is not implemented with details one what each method should be doing

---
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.migration.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
+@Service
+public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
+    @Override
+    public Predicate<CaseDetails> accepts() {
+        // Implement filter here that selects the cases to be migrated
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public Map<String, Object> migrate(Map<String, Object> data) {
+        // Populate a map here with data that wants to be present when connecting with the callback service
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+}
