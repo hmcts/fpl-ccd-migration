7c69b891af3b83ebb953dc485abd252f30ee539e
Ashley Wong <ashley.wong@justice.gov.uk>
2022-08-15 09:41:52 +0100

    Skip migrating case data with SearchCriteria

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -4,19 +4,25 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 @Component
 public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
 
-    private static final String MIGRATION_ID = "YOUR_MIGRATION_ID_HERE";
+    private static final String MIGRATION_ID = "DFPL-702";
 
     @Override
     public Predicate<CaseDetails> accepts() {
-        /*
-         Implement filter here that selects the cases to be migrated.
-        */
-        throw new UnsupportedOperationException("not yet implemented");
+        return new Predicate<CaseDetails>() {
+            @Override
+            public boolean test(CaseDetails caseDetails) {
+                System.out.println("caseDetails.getData().containsKey(\"SearchCriteria\") = " +
+                        caseDetails.getData().containsKey("SearchCriteria")
+                    );
+                return !caseDetails.getData().containsKey("SearchCriteria");
+            }
+        };
     }
 
     @Override
