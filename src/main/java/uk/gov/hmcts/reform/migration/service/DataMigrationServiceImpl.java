0b39408eebaf4c0199590b40816112a3b5dcf18f
Ashley Wong <ashley.wong@justice.gov.uk>
2023-02-02 12:12:10 +0000

    DFPL-1072 (Migration) Court codes for BAD + BHC are incorrect on existing cases

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -4,19 +4,17 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 @Component
 public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
 
-    private static final String MIGRATION_ID = "YOUR_MIGRATION_ID_HERE";
+    private static final String MIGRATION_ID = "DFPL-1072";
 
     @Override
     public Predicate<CaseDetails> accepts() {
-        /*
-         Implement filter here that selects the cases to be migrated.
-        */
-        throw new UnsupportedOperationException("not yet implemented");
+        return caseDetails -> Optional.ofNullable(caseDetails).isPresent();
     }
 
     @Override
