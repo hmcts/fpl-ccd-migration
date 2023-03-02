920b67a48ce29b92b371a5af2005fb2b6dad78f1
SomeshRA <somesh.acharya1@hmcts.net>
2023-03-02 15:47:24 +0000

    DFPL-1240: Updated tests

--- src/test/java/uk/gov/hmcts/reform/migration/auth/AuthUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/migration/auth/AuthUtilTest.java
@@ -7,17 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AuthUtilTest {
 
     @Test
-    public void shouldGetBearToken() {
+   void shouldGetBearToken() {
         assertThat(AuthUtil.getBearerToken("aaaa")).isEqualTo("Bearer aaaa");
     }
 
     @Test
-    public void shouldReturnGetBearToken() {
+    void shouldReturnGetBearToken() {
         assertThat(AuthUtil.getBearerToken("Bearer aaaa")).isEqualTo("Bearer aaaa");
     }
 
     @Test
-    public void shouldReturnBlankToken() {
+    void shouldReturnBlankToken() {
         assertThat(AuthUtil.getBearerToken("")).isEqualTo("");
     }
 }
