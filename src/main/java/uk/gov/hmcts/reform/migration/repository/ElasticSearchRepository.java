0bbde909f74bf97f84a380154d1735921f2a9cf0
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-05-12 09:12:24 +0100

    unused import

--- src/main/java/uk/gov/hmcts/reform/migration/repository/ElasticSearchRepository.java
+++ src/main/java/uk/gov/hmcts/reform/migration/repository/ElasticSearchRepository.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.migration.repository;
 
-import feign.FeignException;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
