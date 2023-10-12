8d4488455db6105c1c900939f7a4b294f8a41573
Ashley Wong <ashley.wong@justice.gov.uk>
2023-10-12 10:38:54 +0100

    DFPL-1802

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -43,7 +43,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-1352", this::triggerOnlyMigration,
         "DFPL-AM", this::triggerOnlyMigration,
         "DFPL-AM-Rollback", this::triggerOnlyMigration,
-        "DFPL-1793", this::triggerOnlyMigration
+        "DFPL-1793", this::triggerOnlyMigration,
+        "DFPL-1802", this::triggerOnlyMigration
     );
 
     private final Map<String, EsQuery> queries = Map.of(
