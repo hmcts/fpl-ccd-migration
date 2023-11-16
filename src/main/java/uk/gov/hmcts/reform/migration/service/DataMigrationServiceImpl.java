9092b7b7949b85bc1a38106a85d06e60faa2f536
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2023-11-16 17:47:26 +0000

    update migration tools

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -46,6 +46,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-1883", this::triggerOnlyMigration,      
         "DFPL-CFV", this::triggerOnlyMigration,
         "DFPL-CFV-Rollback", this::triggerOnlyMigration
+        "DFPL-CFV-Rollback", this::triggerOnlyMigration,
+        "DFPL-1887", this::triggerOnlyMigration
     );
 
     private final Map<String, EsQuery> queries = Map.of(
