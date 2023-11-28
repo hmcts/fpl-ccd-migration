f6be3c921e2e22b9aa3008465bbbd3e5484444de
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2023-11-28 16:04:03 +0000

    update migration tool (#192)

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -42,11 +42,9 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-1124Rollback", this::run1124Rollback,
         "DFPL-AM", this::triggerOnlyMigration,
         "DFPL-AM-Rollback", this::triggerOnlyMigration,
-        "DFPL-1850", this::triggerOnlyMigration,
-        "DFPL-1899", this::triggerOnlyMigration,
+        "DFPL-1915", this::triggerOnlyMigration,
         "DFPL-CFV", this::triggerOnlyMigration,
-        "DFPL-CFV-Rollback", this::triggerOnlyMigration,
-        "DFPL-1887", this::triggerOnlyMigration
+        "DFPL-CFV-Rollback", this::triggerOnlyMigration
     );
 
     private final Map<String, EsQuery> queries = Map.of(
