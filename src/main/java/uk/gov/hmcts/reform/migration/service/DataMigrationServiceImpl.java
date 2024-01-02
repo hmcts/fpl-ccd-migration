5cebbf0e8bbf2aafc116056b21b907aae67c4ac8
Ashley Wong <ashley.wong@justice.gov.uk>
2024-01-02 23:34:57 +0000

    DFPL-1957/DFPL-1993 Migration script to remove position statements.

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -43,8 +43,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         "DFPL-CFV-dry", this::triggerOnlyMigration,
         "DFPL-1934", this::run1934,
         "DFPL-1855", this::run1855,
-        "DFPL-1954", this::triggerOnlyMigration,
-        "DFPL-1948", this::triggerOnlyMigration
+        "DFPL-1957", this::triggerOnlyMigration,
+        "DFPL-1993", this::triggerOnlyMigration
     );
 
     private final Map<String, EsQuery> queries = Map.of(
