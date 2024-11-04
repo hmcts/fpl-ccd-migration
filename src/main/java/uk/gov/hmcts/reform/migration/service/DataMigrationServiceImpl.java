2649d6abdc10e249c330812fd056245fd02b910c
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2024-11-04 14:29:18 +0000

    DFPL-2585: Setup migration, cleanup old queries

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -1,13 +1,10 @@
 package uk.gov.hmcts.reform.migration.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.domain.exception.CaseMigrationSkippedException;
 import uk.gov.hmcts.reform.migration.query.BooleanQuery;
 import uk.gov.hmcts.reform.migration.query.EsQuery;
 import uk.gov.hmcts.reform.migration.query.ExistsQuery;
@@ -15,18 +12,15 @@ import uk.gov.hmcts.reform.migration.query.Filter;
 import uk.gov.hmcts.reform.migration.query.MatchQuery;
 import uk.gov.hmcts.reform.migration.query.Must;
 import uk.gov.hmcts.reform.migration.query.MustNot;
-import uk.gov.hmcts.reform.migration.query.TermQuery;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static java.util.Objects.requireNonNull;
-import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Slf4j
 @Component
@@ -34,30 +28,21 @@ import static org.springframework.util.ObjectUtils.isEmpty;
 public class DataMigrationServiceImpl implements DataMigrationService<Map<String, Object>> {
 
     public static final String COURT = "court";
-    private final ObjectMapper objectMapper;
     private final Map<String, Function<Map<String, Object>, Map<String, Object>>> migrations = Map.of(
         "DFPL-log", this::triggerOnlyMigration,
-        "DFPL-2579", this::triggerOnlyMigration
+        "DFPL-2585", this::triggerOnlyMigration
         );
 
     private final Map<String, EsQuery> queries = Map.of(
-        "DFPL-log", this.query1934(),
-        "DFPL-1934", this.query1934(),
-        "DFPL-1233", this.query1233(),
-        "DFPL-1233Rollback", this.query1233(),
-        "DFPL-AM", this.queryAM(),
-        "DFPL-AM-Rollback", this.queryAM()
+        "DFPL-2585", this.closedCases()
     );
 
-    private EsQuery queryAM() {
-        final MatchQuery openCases = MatchQuery.of("state", "Open");
-        final MatchQuery deletedCases = MatchQuery.of("state", "Deleted");
-        final MatchQuery returnedCases = MatchQuery.of("state", "RETURNED");
-        final MatchQuery closedCases = MatchQuery.of("state", "CLOSED");
+    private EsQuery closedCases() {
+        final MatchQuery closedState = MatchQuery.of("state", "CLOSED");
 
         return BooleanQuery.builder()
-            .mustNot(MustNot.builder()
-                .clauses(List.of(openCases, deletedCases, returnedCases, closedCases))
+            .must(Must.builder()
+                .clauses(List.of(closedState))
                 .build())
             .build();
     }
@@ -95,18 +80,6 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
         return migrations.get(migrationId).apply(data);
     }
 
-    private EsQuery query1934() {
-        // Can skip Open + Deleted cases as these won't have the field on them
-        final MatchQuery openCases = MatchQuery.of("state", "Open");
-        final MatchQuery deletedCases = MatchQuery.of("state", "Deleted");
-
-        return BooleanQuery.builder()
-            .mustNot(MustNot.builder()
-                .clauses(List.of(openCases, deletedCases))
-                .build())
-            .build();
-    }
-
     private EsQuery topLevelFieldExistsQuery(String field) {
         return BooleanQuery.builder()
             .filter(Filter.builder()
@@ -125,84 +98,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
             .build();
     }
 
-    private EsQuery query1855() {
-        return BooleanQuery.builder()
-            .filter(Filter.builder()
-                .clauses(List.of(
-                    BooleanQuery.builder()
-                        .must(Must.of(ExistsQuery.of("data.court")))
-                        .must(Must.of(TermQuery.of("data.court.code", "270")))
-                        .build()
-                ))
-                .build())
-            .build();
-    }
-
-    private EsQuery query1233() {
-        return BooleanQuery.builder()
-            .filter(Filter.builder()
-                .clauses(List.of(
-                    BooleanQuery.builder()
-                        .must(Must.of(ExistsQuery.of("data.hearingDetails")))
-                        .build()
-                ))
-                .build())
-            .build();
-    }
-
     private Map<String, Object> triggerOnlyMigration(Map<String, Object> data) {
         // do nothing
-        return new HashMap<>();
-    }
-
-    private Map<String, Object> run1934(Map<String, Object> data) {
-        // do nothing
-        if (isEmpty(data.get("changeOrganisationRequestField"))) {
-            throw new CaseMigrationSkippedException("Skipping case, changeOrganisationRequestField is empty");
-        }
-        return new HashMap<>();
-    }
-
-    @SuppressWarnings("unchecked")
-    private boolean processHearingDetails(Object hearingDetails, Predicate<Map<String, Object>> checkHearingDetails) {
-        if (Objects.nonNull(hearingDetails)) {
-            List<Map<String, Object>> detailsMap = objectMapper.convertValue(hearingDetails, new TypeReference<>() {});
-            return detailsMap.stream()
-                .map(hearingDetail -> hearingDetail.get("value"))
-                .filter(Objects::nonNull)
-                .map(value -> (Map<String, Object>) value)
-                .anyMatch(checkHearingDetails);
-        }
-        return false;
-    }
-
-    private Map<String, Object> run1233(Map<String, Object> data) throws CaseMigrationSkippedException {
-        Object hearingDetails = data.get("hearingDetails");
-        boolean hasOtherTypeHearings = processHearingDetails(hearingDetails, hearingDetail -> hearingDetail.get("type")
-            .equals("OTHER"));
-
-        Object cancelledHearingDetails = data.get("cancelledHearingDetails");
-        boolean hasOtherTypeCancelledHearings = processHearingDetails(cancelledHearingDetails, hearingDetail ->
-            "OTHER".equals(hearingDetail.get("type")));
-
-        if (!hasOtherTypeHearings && !hasOtherTypeCancelledHearings) {
-            throw new CaseMigrationSkippedException("Skipping case - no hearings with type OTHER found.");
-        }
-        return new HashMap<>();
-    }
-
-    private Map<String, Object> run1233Rollback(Map<String, Object> data) throws CaseMigrationSkippedException {
-        Object hearingDetails = data.get("hearingDetails");
-        boolean hasNonEmptyTypeDetails = processHearingDetails(hearingDetails, hearingDetail ->
-            Objects.nonNull((hearingDetail.get("typeDetails"))));
-
-        Object cancelledHearingDetails = data.get("cancelledHearingDetails");
-        boolean hasNonEmptyTypeCancelledDetails = processHearingDetails(cancelledHearingDetails, hearingDetail ->
-            Objects.nonNull(hearingDetail.get("typeDetails")));
-
-        if (!hasNonEmptyTypeDetails && !hasNonEmptyTypeCancelledDetails) {
-            throw new CaseMigrationSkippedException("Skipping case - no hearings with type details set.");
-        }
         return new HashMap<>();
     }
 }
