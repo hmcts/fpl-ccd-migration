026603b4d35daba062232c673c3b8a9882805ddf
Alistair Osborne <alistairbatemanosborne@gmail.com>
2024-11-21 12:12:55 +0000

    DFPl-2610: Remove legal basis information (#249)

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -30,9 +30,7 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
     public static final String COURT = "court";
     private final Map<String, Function<Map<String, Object>, Map<String, Object>>> migrations = Map.of(
         "DFPL-log", this::triggerOnlyMigration,
-        "DFPL-2585", this::triggerOnlyMigration,
-        "DFPL-2597", this::triggerOnlyMigration,
-        "DFPL-2605", this::triggerOnlyMigration
+        "DFPL-2610", this::triggerOnlyMigration
         );
 
     private final Map<String, EsQuery> queries = Map.of(
