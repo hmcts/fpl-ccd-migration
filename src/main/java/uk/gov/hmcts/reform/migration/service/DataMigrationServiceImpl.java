586e8d5ae06f20b379f36ed442bda17c9dd2b3e2
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-11-28 17:21:38 +0000

    Add migration to remove text from legal basis for case.

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -30,7 +30,8 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
     public static final String COURT = "court";
     private final Map<String, Function<Map<String, Object>, Map<String, Object>>> migrations = Map.of(
         "DFPL-log", this::triggerOnlyMigration,
-        "DFPL-2610", this::triggerOnlyMigration
+        "DFPL-2610", this::triggerOnlyMigration,
+        "DFPL-2613", this::triggerOnlyMigration
         );
 
     private final Map<String, EsQuery> queries = Map.of(
