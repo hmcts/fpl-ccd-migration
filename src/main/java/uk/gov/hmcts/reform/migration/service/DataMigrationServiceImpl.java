86c413e9e060d00f6dd8737a673ad8d831775ad5
Qasim Nawaz <96177697+qasimnawaz-moj@users.noreply.github.com>
2025-01-15 12:47:29 +0000

    DFPL-2635: Add both migrations to migrations map (#254)

--- src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/DataMigrationServiceImpl.java
@@ -38,7 +38,9 @@ public class DataMigrationServiceImpl implements DataMigrationService<Map<String
     public static final String COURT = "court";
     private final Map<String, Function<CaseDetails, Map<String, Object>>> migrations = Map.of(
         "DFPL-log", this::triggerOnlyMigration,
-        "DFPL-2572", this::triggerTtlMigration
+        "DFPL-2572", this::triggerTtlMigration,
+        "DFPL-2635", this::triggerOnlyMigration,
+        "DFPL-2636", this::triggerOnlyMigration
         );
 
     private final Map<String, EsQuery> queries = Map.of(
